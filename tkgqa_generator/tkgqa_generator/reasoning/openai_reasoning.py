567498369e28fd1caa8ddf59e6e6c88d3beabaf8
PascalSun <pascalsun23@gmail.com>
2024-06-17 22:13:05 +0800

    test reasoning, looks very good

---
+++ tkgqa_generator/tkgqa_generator/reasoning/openai_reasoning.py
@@ -0,0 +1,36 @@
+from openai import OpenAI
+
+from tkgqa_generator.utils import get_logger
+
+logger = get_logger(__name__)
+
+client = OpenAI()
+
+
+def reasoning_temporal_questions(prompt: str):
+    """
+
+    Args:
+        prompt (str): The question to reason.
+    """
+    try:
+        logger.info(f"Reasoning the question: {prompt}")
+        res = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {
+                    "role": "system",
+                    "content": """You are an expert on reasoning temporal questions.
+                                  Give us the answer to the following question.
+                                  """,
+                },
+                {
+                    "role": "user",
+                    "content": prompt,
+                },
+            ],
+        )
+        return res.choices[0].message.content
+    except Exception as e:
+        logger.error(e)
+        return ""
--- tkgqa_generator/tkgqa_generator/reasoning/reasoning.py
+++ tkgqa_generator/tkgqa_generator/reasoning/reasoning.py
@@ -1,20 +1,27 @@
-from tkgqa_generator.utils import get_logger, timer
-import pandas as pd
-from sqlalchemy import create_engine, text
+import argparse
+import random
 from multiprocessing import cpu_count
+
+import pandas as pd
+import tqdm
+from sqlalchemy import create_engine
+
+from tkgqa_generator.reasoning.openai_reasoning import reasoning_temporal_questions
+from tkgqa_generator.utils import get_logger, timer
 
 logger = get_logger(__name__)
 
 
 class TemporalReasoningEvaluator:
-    def __init__(self,
-                 table_name: str,
-                 host: str,
-                 port: int,
-                 user: str,
-                 password: str,
-                 db_name: str = "tkgqa",
-                 ):
+    def __init__(
+        self,
+        table_name: str,
+        host: str,
+        port: int,
+        user: str,
+        password: str,
+        db_name: str = "tkgqa",
+    ):
         """
 
         Args:
@@ -31,24 +38,60 @@ class TemporalReasoningEvaluator:
         self.user = user
         self.password = password
         self.db_name = db_name
-        self.engine = create_engine(f"postgresql://{user}:{password}@{host}:{port}/{db_name}")
+        self.engine = create_engine(
+            f"postgresql://{user}:{password}@{host}:{port}/{db_name}"
+        )
         self.table_name = table_name
         with timer(logger, "Loading the table"):
             self.qa_df = pd.read_sql_table(table_name, self.engine)
-            logger.info(f"Loaded {len(self.qa_df)} records from the table {table_name}.")
+            logger.info(
+                f"Loaded {len(self.qa_df)} records from the table {table_name}."
+            )
 
         self.max_workers = cpu_count
 
     def evaluate(self):
-        pass
+        """
+        Evaluate the temporal reasoning. via the LLM
+        Given the question, the LLM will generate the answer.
+        """
+        random_index = random.randint(0, len(self.qa_df))
+
+        for index, row in tqdm.tqdm(self.qa_df.iterrows(), total=len(self.qa_df)):
+            if index != random_index:
+                continue
+            question = row["question"]
+            events = row["events"]
+            logger.info(f"Question: {question}")
+            logger.info(f"Events: {events}")
+            logger.info(f"Answer: {row['answer']}")
+            reasoning_prompt = (
+                f"Given the events: {events}, reason the following question: {question}"
+            )
+
+            res = reasoning_temporal_questions(reasoning_prompt)
+
+            logger.info(f"Reasoning result: {res}")
+            break
 
 
 if __name__ == "__main__":
-    table_name = "unified_kg_cron_questions"
-    host = "localhost"
-    port = 5433
-    user = "tkgqa"
-    password = "tkgqa"
-    db_name = "tkgqa"
-    evaluator = TemporalReasoningEvaluator(table_name, host, port, user, password, db_name)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--table_name", type=str, default="unified_kg_cron_questions")
+    parser.add_argument("--host", type=str, default="localhost")
+    parser.add_argument("--port", type=int, default=5433)
+    parser.add_argument("--user", type=str, default="tkgqa")
+    parser.add_argument("--password", type=str, default="tkgqa")
+    parser.add_argument("--db_name", type=str, default="tkgqa")
+    args = parser.parse_args()
+
+    evaluator = TemporalReasoningEvaluator(
+        table_name=args.table_name,
+        host=args.host,
+        port=args.port,
+        user=args.user,
+        password=args.password,
+        db_name=args.db_name,
+    )
     evaluator.evaluate()
